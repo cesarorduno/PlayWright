ba6ce301ccfbfe165eda752eaeab398ce9551c8d
CesarOrduno <cesarordunoalbarran@yahoo.com.mx>
2023-03-06 21:49:45 -0600

    Test-Regressions.xml

---
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -0,0 +1,28 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qa.opencart.base.BaseTest;
+import com.qa.opencart.constants.AppConstants;
+
+public class LoginPageTest extends BaseTest{
+
+	@Test(priority = 1)
+	public void loginPageNavigation() {
+		loginPage = homePage.navigateToLoginPage();
+		String actLoginPageTitle = loginPage.getLoginPageTitle();
+		System.out.println("page act title: " + actLoginPageTitle);
+		Assert.assertEquals(actLoginPageTitle, AppConstants.LOGIN_PAGE_TITLE);
+	}
+	
+	@Test(priority = 2)
+	public void forgotPPwsLinkExistTest() {
+		Assert.assertTrue(loginPage.isForgotPwsLinkExist());
+	}
+	
+	@Test(priority = 3)
+	public void appLoginTest() {
+		Assert.assertTrue(loginPage.doLogin(prop.getProperty("username").trim(), prop.getProperty("password").trim()));
+	}
+}
